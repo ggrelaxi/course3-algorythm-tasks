dba8c4784d046e905d121624a00837278b15bf0d
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-04-09 18:05:15 +0300

    fix code to test

--- task3.py
+++ task3.py
@@ -17,20 +17,31 @@ def ConquestCampaign(N, M, L, battalion):
         if i % 2 == 0:
             singleDesant.append(battalion[i])
             singleDesant.append(battalion[i + 1])
-            desantCords.append(singleDesant)
-    
+            if singleDesant not in desantCords:
+                desantCords.append(singleDesant)
+
     #первая высадка
     for i in range(len(desantCords)):
         x, y = desantCords[i]
-        if (x - 1) < N and (y - 1) < M:
-            map[x - 1][y - 1] = "win"
+        map[x - 1][y - 1] = "win"
     
+
+    for i in range(len(map)):
+        victory = True
+        for j in range(len(map[i])):
+            if map[i][j] == "bunt":
+                victory = False
+                break
+        if victory == False:
+            break
     days += 1
+
+    if victory == True:
+        return days
 
     #Дальнейший захват
     while victory == False:
         for i in range(len(desantCords)):
-            singleDesant = []
             x, y = desantCords[i]
             desantCords.append([x, y + 1])
             desantCords.append([x, y - 1])
@@ -39,17 +50,16 @@ def ConquestCampaign(N, M, L, battalion):
 
         for i in range(len(desantCords)):
             x, y = desantCords[i]
-            if (x - 1) < N and (y - 1) < M:
+            if x <= N and y <= M:
                 map[x - 1][y - 1] = "win"
 
-        #Если после текущего дня, не осталось клетов с бунтовщиками - победа!!
+    #Если после текущего дня, не осталось клеток с бунтовщиками - победа!!
         for i in range(len(map)):
             victory = True
             for j in range(len(map[i])):
                 if map[i][j] == "bunt":
                     victory = False
                     break
-                break
             
         days += 1
 
--- task3_test.py
+++ task3_test.py
@@ -6,6 +6,7 @@ class ConquestCampaignTest(unittest.TestCase):
         self.assertEqual(ConquestCampaign(3, 4, 2, [2,2, 3,4]), 3)
         self.assertEqual(ConquestCampaign(3, 4, 2, [2,2, 2,2]), 4)
         self.assertEqual(ConquestCampaign(4, 5, 3, [2,2, 3,4, 2,5]), 4)
+        self.assertEqual(ConquestCampaign(2,3,6,[1,1,1,2,1,3,2,1,2,2,2,3]), 1)
 
 if __name__ == "__main__":
     unittest.main()
