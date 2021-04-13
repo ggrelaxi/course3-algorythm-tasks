e01273a0b79e19bf130844c6179d97e9c48afce9
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-04-13 14:51:24 +0300

    fix tests

--- task6.py
+++ task6.py
@@ -1,12 +1,49 @@
 def PatternUnlock(N, hits):
     result = 0
-    for i in range(N):
-        result += hits[i]
+    hypo = 1.4142135623730951
+    matrix = [[6,1,9], [5,2,8], [4,3,7]]
 
-    data = list(str(result))
 
-    for i in range(len(data)):
-        if data[i] == 0:
-            del data[i]
+    #матрица
+    for i in range(len(matrix)):
+        print(matrix[i])
+
+    #координаты текущей точки
+    def getCords(value):
+        cords = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == value:
+                    cords.append(i)
+                    cords.append(j)
+                    break
+        return cords
+
+    def getNeighbors(cords):
+        x, y = cords
+        lineNeighbors = []
+        lineNeighbors.append([x, y + 1])
+        lineNeighbors.append([x, y - 1])
+        lineNeighbors.append([x - 1, y])
+        lineNeighbors.append([x + 1, y])
+
+        return [lineNeighbors]
+
+
+    for i in range(N - 1):
+        actualCords = getCords(hits[i])
+        nextCords = getCords(hits[i + 1])
+        line = getNeighbors(actualCords)
+
+        if (nextCords in line):
+            result += 1
+        else:
+            result += hypo
     
-    return "".join(data)
+    data = list(str(int(round(result, 5) * 100000)))
+    return "".join([num for num in data if num != 0])
+
+        
+
+
+PatternUnlock(3, [1,2,9])
--- task6_test.py
+++ task6_test.py
@@ -3,7 +3,8 @@ from task6 import PatternUnlock
 
 class PatternUnlockTest(unittest.TestCase):
     def successTest(self):
-        self.assertEqual(PatternUnlock(9, [1,2,3,4,5,6,7,8,9]), "45")
+        self.assertEqual(PatternUnlock(3, [1,2,9]), "241421")
+        self.assertEqual(PatternUnlock(9, [1,2,3,4,5,6,2,7,8,9]), "982843")
 
 if __name__ == "__main__":
     unittest.main()
