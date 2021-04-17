35840a3767bffa41763ad045fa1dca7b9a17bd5c
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-04-17 21:06:49 +0300

    fix cycle algo

--- task8.py
+++ task8.py
@@ -2,7 +2,9 @@ def SumOfThe(N, data):
     for i in range(N):
         actual = data[i]
         summ = 0
-        for j in range(i, N):
+        for j in range(0, N):
+            if j == i:
+                continue
             summ += data[j]
         if actual == summ:
             return summ
--- task8_test.py
+++ task8_test.py
@@ -4,6 +4,7 @@ from task8 import SumOfThe
 class SumOfTheTest(unittest.TestCase):
     def testSuccess(self):
         self.assertEqual(SumOfThe(7, [100, -50, 10, -25, 90, -35, 90]), 90)
+        self.assertEqual(SumOfThe(5, [10, -25, -45, -35, 5]), -45)
 
 if __name__ == "__main__":
     unittest.main()
