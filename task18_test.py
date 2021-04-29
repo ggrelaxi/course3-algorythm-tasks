90b829735e02a76abc90487ac816379ce61776d8
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-04-29 14:19:20 +0300

    add time test

--- task18_test.py
+++ task18_test.py
@@ -1,9 +1,15 @@
 import unittest
 from task18 import MisterRobot
+import time
 
 class MisterRobotTest(unittest.TestCase):
     def testSuccess(self):
         self.assertEqual(MisterRobot(7, [1,3,4,5,6,2,7]), True)
-
+        self.assertEqual(MisterRobot(7, [1,4,3,5,7,6,2]), True)
+    def testTime(self):
+        startTime = time.time()
+        MisterRobot(7, [1,4,3,5,7,6,2])
+        endTime = time.time()
+        self.assertLess((endTime - startTime), 1)
 if __name__ == "__main__":
     unittest.main()
