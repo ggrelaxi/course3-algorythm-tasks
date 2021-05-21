819109b54968b399a1ddfb7f3453e1c0b4bc5a0f
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-05-21 10:28:53 +0300

    delete print

--- task24_test.py
+++ task24_test.py
@@ -6,7 +6,6 @@ class MatrixTurnTest(unittest.TestCase):
         matrix = ["123456", "234567", "345678", "456789"]
         turn = ["212345", "343456", "456767", "567898"]
         MatrixTurn(matrix, 4, 6, 1)
-        print(matrix, turn)
         self.assertEqual(matrix, turn)
 
 if __name__ == "__main__":
