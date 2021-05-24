09e51e45456e376b0be774d975979d614ce540f7
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-05-24 14:29:26 +0300

    fix range

--- task25.py
+++ task25.py
@@ -1,32 +1,24 @@
+def cycles(arr):
+        result = []
+        for i in range((len(arr) - 1) + 1):
+            for j in range((len(arr) - i - 1) + 1):
+                k = i + j
+                max = arr[j]
+                
+                for m in range(j, k + 1):
+                    if arr[m] > max:
+                        max = arr[m]
+                
+                result.append(max)
+        print(result)
+        return result
+
 def TransformTransform(A, N):
-    result = []
+    first = cycles(A)
+    second = cycles(first)
 
-    for i in range(len(A) - 1):
-        for j in range(len(A) - i - 1):
-            k = i + j
-            
-            max = A[j]
-            for m in range(k):
-                if A[m] > max:
-                    max = A[m]
-            
-            result.append(max)
-    
-    secondResult = []
+    sum = 0
+    for i in range(len(second)):
+        sum += second[i]
 
-    for g in range(len(result) - 1):
-        for x in range(len(result) - g - 1):
-            k = g + x
-            
-            max = result[x]
-            for q in range(k):
-                if result[q] > max:
-                    max = result[q]
-            
-            secondResult.append(max)
-
-    summ = 0
-    for y in range(len(secondResult)):
-        summ += secondResult[y]
-
-    return summ % 2 == 0
+    return sum % 2 == 0
--- task25_test.py
+++ task25_test.py
@@ -3,7 +3,7 @@ from task25 import TransformTransform
 
 class TranformTransformTest(unittest.TestCase):
     def testSuccess(self):
-        self.assertEqual(TransformTransform([1,2,3,4,5,6,7], 7), True)
+        self.assertEqual(TransformTransform([3,2,1], 3), True)
 
 if __name__ == "__main__":
     unittest.main()
