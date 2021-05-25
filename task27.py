59d493d53456bdbcc1b23c1fbc067ed7a3b49fd5
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-05-25 17:11:13 +0300

    add files and finish task

---
+++ task27.py
@@ -0,0 +1,40 @@
+import copy
+
+def Football(F, N):
+    for i in range(1, 3):
+        tempArr = copy.copy(F)
+        change = True
+
+        while change == True:
+            change = False
+            for j in range(N - 1):
+                current = tempArr[j]
+
+                if current > tempArr[j + 1]:
+                    temp = current
+                    tempArr[j] = tempArr[j + 1]
+                    tempArr[j + 1] = temp
+                    change = True
+    
+
+        if i == 1:
+            temp1 = copy.copy(F)
+            temp1.reverse()
+            
+            if temp1 == tempArr:
+                return True
+
+        if i == 2:
+            temp2 = copy.copy(F)
+
+            for y in range(N - 1):
+                for x in range(1, N):
+                    current = temp2[y]
+                    next = temp2[x]
+                    temp2[y] = next
+                    temp2[x] = current
+
+                    if temp2 == tempArr:
+                        return True
+                    else:
+                        temp2 = copy.copy(F)
---
+++ task27_test.py
@@ -0,0 +1,11 @@
+import unittest
+from task27 import Football
+
+class FootballTest(unittest.TestCase):
+    def testSuccess(self):
+        self.assertEqual(Football([1,3,2], 3), True)
+        self.assertEqual(Football([3,2,1], 3), True)
+        self.assertEqual(Football([9,5,3,7,1], 5), None)
+
+if __name__ == "__main__":
+    unittest.main()
